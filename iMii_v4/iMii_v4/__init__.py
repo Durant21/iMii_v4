dab6286f2ab9e3e8bed0582f45d6af254672f242
Dante Fernandez <durant.crimson@icloud.com>
2018-11-24 14:02:52 -0700

    adding splash page

--- iMii_v4/iMii_v4/__init__.py
+++ iMii_v4/iMii_v4/__init__.py
@@ -6,6 +6,10 @@ def main(global_config, **settings):
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_chameleon')
-        config.include('.routes')
+        # config.include('.routes')
+        config.add_static_view( 'static', 'static', cache_max_age=3600 )
+        config.add_static_view( 'graphics', 'static/graphics' )
+        config.add_route( 'home', '/' )
+        config.add_route( 'splash', '/splash' )
         config.scan()
     return config.make_wsgi_app()
